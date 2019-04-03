e34d0854a8c232349a934a6883be82de03e324d8
sunney1106 <sunney.wang@zilly.com.cn>
2019-04-03 16:24:21 +0800

    prompt

--- background-manage/src/App.js
+++ background-manage/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import "./components/manage.css"
-import Management from "./components/manage"
+import Navleft from "./components/navleft/navleft"
+// import Management from "./components/manage"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Management></Management>
+      <Navleft></Navleft>
+        {/* <Management></Management> */}
       </div>
     );
   }
