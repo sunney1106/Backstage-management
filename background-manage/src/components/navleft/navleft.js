946c8abee10dc431646cdb1794b14f2cfea6ddef
sunney1106 <sunney.wang@zilly.com.cn>
2019-04-04 10:43:51 +0800

    2019/4/4

--- background-manage/src/components/navleft/navleft.js
+++ background-manage/src/components/navleft/navleft.js
@@ -5,26 +5,80 @@ import { Menu, Icon } from 'antd';
 const SubMenu = Menu.SubMenu;
 
 class Navleft extends  Component{
-    rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
-    state = {
-        openKeys: ['sub1'],
-    };
-    
-
+    // let arr = [
+    //     {
+    //         "id":1,
+    //         "tittle":"首页",
+    //         "path":"./home"
+    //     },
+    //     {
+    //         "id":2,
+    //         "tittle":"商品管理",
+    //         "path":"./management",
+    //         "children":[
+    //             {
+    //                 "id":21,
+    //                 "tittle":"猜我喜欢",
+    //                 "path":"./my"
+    //             },
+    //             {
+    //                 "id":22,
+    //                 "tittle":"猜你喜欢",
+    //                 "path":"./you"
+    //             },
+    //             {
+    //                 "id":23,
+    //                 "tittle":"猜他喜欢",
+    //                 "path":"./it"
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         "id":3,
+    //         "tittle":"商品信息",
+    //         "path":"./information"
+    //     },
+    //     {
+    //         "id":4,
+    //         "tittle":"用户信息",
+    //         "path":"./user"
+    //     }
+    // ]
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         arr
+    //     }
+    // }
+    renderNav(){
+        let {arr} = this.state
+        return this.renderMenu(arr)
+    } 
+    renderMenu(arr){
+        return arr.map((item)=>{
+            if(item.children){
+                return (
+                    <SubMenu key={item.key} title={item.title}>
+                        this.renderMenu(item.children)
+                    </SubMenu>
+                )
+            }else{
+                return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+            }
+        })
+    }
     render(){
         return (
           <div className="navleft">
             <div className="header">
-                <img src="../../resource/img/logo.jpg.jpg" alt=""/>
+                <img src="../../resource/img/logo.jpg" alt=""/>
                 111111
             </div>
             <Menu
                 mode="inline"
                 style={{ width: 256 }}
             >
-                <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                    <Menu.Item key="1">Option 1</Menu.Item>
-                </SubMenu>
+                this.renderNav()
             </Menu>
         </div>
         )
