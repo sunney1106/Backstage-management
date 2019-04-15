04bd1e2582d4a45a10ad54bceb4f13f087de3a82
sunney1106 <sunney.wang@zilly.com.cn>
2019-04-15 09:48:12 +0800

    login jump

--- background-manage/src/pages/login/login.js
+++ background-manage/src/pages/login/login.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import {Form, Icon, Input, Button, Checkbox,Card } from 'antd';
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
 import '@/resource/style/login.css'
   
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
@@ -12,26 +16,51 @@ class Login extends Component {
             }
         });
     }
-    handLogin(){
-        alert(1)
+   
+    handUser = (e) =>{
+        this.setState({
+            userName : e.target.value
+        })
     }
+    handPassword = (e)=>{
+        this.setState({
+            password : e.target.value
+        })
+    }
+
+    handLogin = () =>{
+    var userName = this.state.userName;
+    var password = this.state.password;
+    var reg = /^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$/;
+    if(userName !== undefined && reg.test(password) && password !== undefined){
+        this.props.history.push('./admin/home')
+    }else{
+        return
+    }
+    }
+
+
+
   render(){
     const { getFieldDecorator } = this.props.form;
     return (
-      <Card className="login" title="BackStage Manage">
+      <div className="login" >
+        <div className = "login_header">Background</div>
            <Form onSubmit={this.handleSubmit} className="login-form">
                 <Form.Item>
                     {getFieldDecorator('userName', {
                          rules: [{ required: true, message: 'Please input your username!' }],
                     })(
-                         <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                         <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" onChange={this.handUser} />
                      )}
                 </Form.Item>
                 <Form.Item>
                      {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Please input your Password!' }],
+                        rules: [
+                            { required: true, message: 'Please input your Password!',pattern:/^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$/,max:12,min:6 }
+                    ],
                      })(
-                         <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                         <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password"  onChange={this.handPassword} />
                      )}
                 </Form.Item>
                 <Form.Item>
@@ -46,7 +75,7 @@ class Login extends Component {
                     </Button>
                 </Form.Item>
         </Form>
-      </Card>
+      </div>
     );
   }
 }
